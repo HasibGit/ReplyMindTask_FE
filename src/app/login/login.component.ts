0c02d31944707db4fd5a01c91ac340f3be2c8bc5
HasibGit <hasibullah04@gmail.com>
2023-11-09 21:13:21 +0600

    basic login form setup done

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+  isLoading = false;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this.initLoginForm();
   }
 
+  initLoginForm() {
+    this.loginForm = this.fb.group({
+      Email: ['', [Validators.required, Validators.email]],
+      Password: ['', [Validators.required]],
+    });
+  }
+
+  login() {
+    this.isLoading = true;
+    console.log(this.loginForm.value);
+    this.isLoading = false;
+  }
 }
