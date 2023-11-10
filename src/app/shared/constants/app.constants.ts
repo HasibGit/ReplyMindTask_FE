85c6a8909d36aa564d50d310998b9a92f1179210
HasibGit <hasibullah04@gmail.com>
2023-11-10 15:45:30 +0600

    working on professional information form

--- src/app/shared/constants/app.constants.ts
+++ src/app/shared/constants/app.constants.ts
@@ -7,3 +7,29 @@ export const USER_PASSWORD_CRITERIA: string[] = [
   'At least one number',
   'At least one special character',
 ];
+
+export const USER_PROFESSIONS: string[] = [
+  'Marketing Professional',
+  'Entrepreneur',
+  'Content Creator',
+];
+
+export const AREAS_OF_INTEREST_RELEVANT_TO_PROFESSION: {
+  [key: string]: string[];
+} = {
+  'Marketing Professional': [
+    'Growth Marketing',
+    'Digital Marketing',
+    'Product Marketing',
+    'Paid Marketing',
+    'Organic Marketing',
+  ],
+  Entrepreneur: [
+    'Startup Enthusiast',
+    'SME',
+    'Product Enthusiast',
+    'Product Leader',
+    'Product Owner',
+  ],
+  'Content Creator': ['Youtube', 'Twitch', 'Twitter', 'Facebook', 'TikTok'],
+};
