39a110a7d7db0c4b22ced545ed1a9d836a16b73b
HasibGit <hasibullah04@gmail.com>
2023-11-11 15:07:05 +0600

    added endpoint constants

---
+++ src/app/shared/constants/api-endpoints.ts
@@ -0,0 +1,8 @@
+export const API_ENDPOINTS = {
+  SIGN_UP: '/reply-mind/api/signup',
+  LOGIN: '/reply-mind/api/login',
+  UPDATE_USER: '/reply-mind/api/update-user',
+  DELETE_USER: '/reply-mind/api/delete-user',
+  LOGOUT: '/reply-mind/api/logout',
+  GET_USER_BY_ID: '/reply-mind/api/get-user-by-id',
+};
