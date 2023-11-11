5b5ff0cb51af6db973c536473808cb14f4958246
HasibGit <hasibullah04@gmail.com>
2023-11-11 15:41:18 +0600

    token related methods added to the service

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
+  constructor() {}
 
-  constructor() { }
+  isLoggedIn() {
+    return (
+      sessionStorage.getItem('rm_token') && sessionStorage.getItem('rm_userId')
+    );
+  }
+
+  saveToken(token: string) {
+    sessionStorage.setItem('rm_token', token);
+  }
+
+  getToken() {
+    return sessionStorage.getItem('rm_token') || '';
+  }
+
+  saveUserId(userId: string) {
+    sessionStorage.setItem('rm_userId', userId);
+  }
+
+  getUserId() {
+    return sessionStorage.getItem('rm_userId') || '';
+  }
 }
